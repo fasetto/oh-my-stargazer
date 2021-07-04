38994eb76f636180bf933d63d70f2e8b8a20990b
Serkan Bircan <serkanbircan21@yandex.com>
2021-07-04 16:35:38 +0300

    Update title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Next JS Template</title>
+        <title>Oh My Stargazer</title>
       </Head>
 
       <Component {...pageProps} />
